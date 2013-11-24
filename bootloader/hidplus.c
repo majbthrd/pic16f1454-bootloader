1efcf8f31c66c1df89fbf594712b759536d1f8ec
majbthrd <none@none>
2013-11-24 16:13:27 -0700

    Initial Commit

---
+++ bootloader/hidplus.c
@@ -0,0 +1,475 @@
+/*
+    USB HID bootloader for PIC16F1454/PIC16F1455/PIC16F1459 microcontroller
+
+    Copyright (C) 2013 Peter Lawrence
+
+    Permission is hereby granted, free of charge, to any person obtaining a 
+    copy of this software and associated documentation files (the "Software"), 
+    to deal in the Software without restriction, including without limitation 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+    and/or sell copies of the Software, and to permit persons to whom the 
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in 
+    all copies or substantial portions of the Software.
+
+    The software is for use solely and exclusively on Microchip PIC 
+    Microcontroller products. 
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+    DEALINGS IN THE SOFTWARE.
+*/
+
+#include "./USB/usb.h"
+#include "./USB/usb_function_hid.h"
+
+/* state of configuration words */
+__CONFIG(FOSC_INTOSC & WDTE_OFF & PWRTE_ON & MCLRE_OFF & CP_ON & BOREN_ON & CLKOUTEN_OFF & IESO_OFF & FCMEN_OFF);
+__CONFIG(WRT_HALF & CPUDIV_NOCLKDIV & USBLSCLK_48MHz & PLLMULT_3x & PLLEN_ENABLED & STVREN_ON & BORV_LO & LPBOR_OFF & LVP_OFF);
+
+/* USB data buffers (which are also available to the user code when in HIDHYBRID boot mode) */
+unsigned char RxDataBuffer[64] @0xA0;
+unsigned char TxDataBuffer[64] @0x120;
+
+/* handles used for HID functionality */
+USB_HANDLE USBOutHandle;
+USB_HANDLE USBInHandle;
+
+/* prototyping of external USB descriptors in ROM */
+extern ROM USB_DEVICE_DESCRIPTOR hid_bootloader_device_dsc;
+extern ROM BYTE *ROM HID_USB_CD_Ptr[];
+extern ROM BYTE *ROM HID_USB_SD_Bootloader_Ptr[];
+#ifndef OMIT_HIDHYBRID
+extern ROM USB_DEVICE_DESCRIPTOR hid_cypher_device_dsc;
+extern ROM BYTE *ROM HID_USB_SD_Cypher_Ptr[];
+#endif
+
+/* pointers that pick one of the above external USB descriptors */
+USB_DEVICE_DESCRIPTOR const *pnt_device_dsc;
+const BYTE const **USB_CD_Ptr;
+const BYTE const **USB_SD_Ptr;
+
+/* variable storing boot mode (which determines the code's behavior) */
+static enum
+{
+    BOOTLOADER,
+    USERIMAGE,
+#ifndef OMIT_HIDHYBRID
+    HIDHYBRID
+#endif
+} BootMode = BOOTLOADER;
+
+#ifndef OMIT_HIDHYBRID
+/* flag from USER_USB_CALLBACK_EVENT_HANDLER() to main loop */
+static BOOL msTick = FALSE;
+
+/* XC8 syntax approach to optionally call (not goto) HIDHYBRID user code */
+extern BYTE usercode(BYTE) @ 0x1000;
+#endif
+
+/* centralization of delay code used in USB callbacks */
+static void do_delay(WORD delay_count);
+
+void interrupt ISRCode()
+{
+    /*
+    Microchip's ever varying nomenclature is as follows:
+    the goto (no return address pushed onto stack) is called "ljmp" ( *LONG* jump )
+    but the call (return address pushed onto stack) is called "fcall" ( *FAR* call )
+    */
+    if (USERIMAGE == BootMode)
+    {
+        /* in USERIMAGE boot mode, the user's code will handle returning from the interrupt */
+        asm("ljmp 0x1004"); /* call user code interrupt vector */
+    }
+    else
+    {
+        /* in BOOTLOADER and HIDHYBRID boot modes, this is the complete interrupt handler */
+        USBDeviceTasks();
+    }
+}
+
+int main(void)
+{
+    WORD crc, data;
+    BYTE index, lo, hi;
+    BYTE tx_count;
+    
+    /* enable pull-up on RA3 (for boot mode detection) */
+    OPTION_REGbits.nWPUEN = FALSE;
+    WPUA = (1 << 3);
+
+    /* magic numbers from Microchip */
+    OSCTUNE = 0;
+    OSCCON = 0xFC; /* 16MHz HFINTOSC with 3x PLL enabled (48MHz operation) */
+    ACTCON = 0x90; /* Enable active clock tuning with USB */
+
+    /*
+    pull-up on RA3 needs a moment to do its thing
+    so, now is a good time to spend time computing a CRC of the user-programmable area
+    */
+
+    /* initialize CRC */
+    crc = 0;
+
+    /* use local copy of PMADRH:PMADRL to prevent XC8 compilation issues */
+    lo = 0x00;
+    hi = 0x10;
+
+    for (;;)
+    {
+        PMADRH = hi;
+        PMADRL = lo;
+
+        /* clear CFGS (we're reading Program, not Configuration, Memory) */
+        PMCON1bits.CFGS = FALSE;
+        /* set RD (initiate read) */
+        PMCON1bits.RD = TRUE;
+        /* mandatory two nops */
+        _nop(); _nop();
+
+        /* retrieve result from program memory and put it into a WORD */
+        data = PMDATH;
+        data <<= 8;
+        data += PMDATL;
+
+        /* increment program memory address, and bail if we've just read the last word */
+        lo++;
+        if (0 == lo)
+        {
+            hi++;
+            if (0x20 == hi)
+                break;
+        }
+        
+        /* update CRC over the 14 bits of program memory data */
+        for (index = 0; index < 14; index++)
+        {
+            if ((crc & 0x0001) ^ (data & 0x0001))
+                crc = (crc >> 1) ^ 0x23B1;
+            else
+                crc >>= 1;
+            data >>= 1;
+        }
+    }
+
+    /*
+    whilst we've been busy, pull-up on RA3 has now had a chance to work
+    make determination as to whether to not use bootloader
+    */
+    if (PORTAbits.RA3)
+    {
+        if (data == crc)
+            BootMode = USERIMAGE;
+#ifndef OMIT_HIDHYBRID
+        else if ((data ^ 0x3FFF) == crc)
+            BootMode = HIDHYBRID;
+#endif
+    }
+
+    /* disable pull-up now that determination has been made */
+    OPTION_REGbits.nWPUEN = TRUE;
+    WPUA = 0;
+
+    /* if not in bootloader mode, branch to user code (for initialization or outright different USB code) */
+    if (USERIMAGE == BootMode)
+    {
+        /*
+        in USERIMAGE boot mode, we goto ("LONG jump") the user's code with no expectation of returning
+        doing a call would function, but would waste one entry on the stack
+        */
+        asm("ljmp 0x1000"); /* call user code reset vector */
+    }
+#ifndef OMIT_HIDHYBRID
+    else if (HIDHYBRID == BootMode)
+    {
+        /*
+        in HIDHYBRID mode, we expect the user-provided code to return
+        */
+        usercode(2);
+    }
+#endif
+
+    /* choose USB descriptor based on BootMode */
+    pnt_device_dsc = &hid_bootloader_device_dsc;
+    USB_CD_Ptr = (const BYTE const **)HID_USB_CD_Ptr;
+    USB_SD_Ptr = (const BYTE const **)HID_USB_SD_Bootloader_Ptr;
+#ifndef OMIT_HIDHYBRID
+    if (HIDHYBRID == BootMode)
+    {
+        pnt_device_dsc = &hid_cypher_device_dsc;
+        USB_SD_Ptr = (const BYTE const **)HID_USB_SD_Cypher_Ptr;
+    }
+#endif
+
+    /* initialize the USB driver */
+    USBOutHandle = 0;
+    USBInHandle = 0;
+    USBDeviceInit();
+    USBDeviceAttach();
+
+    for (;;)
+    {
+        // User Application USB tasks
+        if( (USBDeviceState < CONFIGURED_STATE) || (1 == USBSuspendControl) )
+            continue;
+
+        //Check if we have received an OUT data packet from the host
+        if (!HIDRxHandleBusy(USBOutHandle))
+        {
+            /*
+            if tx_count is set to be non-zero by subsequent code, this indicates data
+            of length tx_count is in DataBuffer[] to be passed to the USB driver
+            */
+            tx_count = 0;
+
+            if (!HIDTxHandleBusy(USBInHandle))
+            {
+                /*
+                if the code has reached here, the BootMode is either BOOTLOADER or HIDHYBRID
+                here we branch accordingly (either calling bootloader code or the user's code)
+                */
+#ifndef OMIT_HIDHYBRID
+                if (BOOTLOADER == BootMode)
+#endif
+                {
+                    TxDataBuffer[tx_count++] = RxDataBuffer[0];
+                    TxDataBuffer[tx_count++] = RxDataBuffer[1];
+                    TxDataBuffer[tx_count++] = RxDataBuffer[2];
+
+                    hi = RxDataBuffer[1];
+                    lo = RxDataBuffer[2];
+
+                    switch (RxDataBuffer[0])
+                    {
+                    case 0x80:  /* Read Memory */
+                        do
+                        {
+                            /* set starting Program Memory address */
+                            PMADRH = hi;
+                            PMADRL = lo;
+                            /* clear CFGS (we're reading Program, not Configuration, Memory) */
+                            PMCON1bits.CFGS = FALSE;
+                            /* set RD (initiate read) */
+                            PMCON1bits.RD = TRUE;
+                            /* mandatory two nops */
+                            _nop(); _nop();
+                            /* retrieve result */
+                            TxDataBuffer[tx_count++] = PMDATH;
+                            TxDataBuffer[tx_count++] = PMDATL;
+                            /* increment program memory address */
+                            lo++;
+                            if (0 == lo)
+                                hi++;
+                        } while (tx_count < (32 + 3));
+                        break;
+
+                    case 0x81:  /* Erase Memory */
+                        /* disable global interrupts */
+                        INTCONbits.GIE = FALSE;
+                        /* clear CFGS (we're reading Program, not Configuration, Memory) */
+                        PMCON1bits.CFGS = FALSE;
+                        /* provide Program Memory row address */
+                        PMADRH = hi;
+                        PMADRL = lo;
+                        /* select erase operation */
+                        PMCON1bits.FREE = TRUE;
+                        /* enable write/erase operation */
+                        PMCON1bits.WREN = TRUE;
+                        /* unlock sequence */
+                        PMCON2 = 0x55;
+                        PMCON2 = 0xAA;
+                        PMCON1bits.WR = TRUE;
+                        /* mandatory two nops */
+                        _nop(); _nop();
+                        /* disable write/erase operation */
+                        PMCON1bits.WREN = FALSE;
+                        /* re-enable global interrupts */
+                        INTCONbits.GIE = TRUE;
+                        break;
+
+                    case 0x82:  /* Program Memory */
+                        /* disable global interrupts */
+                        INTCONbits.GIE = FALSE;
+                        /* clear CFGS (we're reading Program, not Configuration, Memory) */
+                        PMCON1bits.CFGS = FALSE;
+                        /* provide Program Memory row address */
+                        PMADRH = hi;
+                        PMADRL = lo;
+                        /* select write operation */
+                        PMCON1bits.FREE = FALSE;
+                        /* load write latches only */
+                        PMCON1bits.LWLO = TRUE;
+                        /* enable write/erase operation */
+                        PMCON1bits.WREN = TRUE;
+
+                        index = 3;
+
+                        for (;;)
+                        {
+                            PMDATH = RxDataBuffer[index++];
+                            PMDATL = RxDataBuffer[index++];
+                            if (index >= (32 + 3))
+                            {
+                                /* write latches to flash */
+                                PMCON1bits.LWLO = FALSE;
+                            }
+                            /* unlock sequence */
+                            PMCON2 = 0x55;
+                            PMCON2 = 0xAA;
+                            PMCON1bits.WR = TRUE;
+                            /* mandatory two nops */
+                            _nop(); _nop();
+                            if (index >= (32 + 3))
+                            {
+                                /* we've finished, so bail */
+                                break;
+                            }
+                            /* increment program memory address */
+                            lo++;
+                            if (0 == lo)
+                                hi++;
+                            PMADRL = lo;
+                        }
+                        /* disable write/erase operation */
+                        PMCON1bits.WREN = FALSE;
+                        /* re-enable global interrupts */
+                        INTCONbits.GIE = TRUE;
+                        break;
+                    }
+                }
+#ifndef OMIT_HIDHYBRID
+                else
+                {
+                    /* call user-provided HIDHYBRID boot mode code */
+                    tx_count = usercode(0);
+                }
+#endif
+            }
+
+            if (tx_count)
+                USBInHandle = HIDTxPacket(HID_EP, (BYTE*)&TxDataBuffer[0], tx_count);
+
+            /* Re-arm the OUT endpoint */
+            USBOutHandle = HIDRxPacket(HID_EP, (BYTE*)&RxDataBuffer, 64);
+        }
+
+#ifndef OMIT_HIDHYBRID
+        if (msTick)
+        {
+            if (HIDHYBRID == BootMode)
+            {
+                usercode(1);
+            }
+            msTick = FALSE;
+        }
+#endif
+    }
+}
+
+// ******************************************************************************************************
+// ************** USB Callback Functions ****************************************************************
+// ******************************************************************************************************
+
+void USBCBSendResume(void)
+{
+    //First verify that the host has armed us to perform remote wakeup.
+    //It does this by sending a SET_FEATURE request to enable remote wakeup,
+    //usually just before the host goes to standby mode (note: it will only
+    //send this SET_FEATURE request if the configuration descriptor declares
+    //the device as remote wakeup capable, AND, if the feature is enabled
+    //on the host (ex: on Windows based hosts, in the device manager 
+    //properties page for the USB device, power management tab, the 
+    //"Allow this device to bring the computer out of standby." checkbox 
+    //should be checked).
+    if(USBGetRemoteWakeupStatus() == TRUE) 
+    {
+        //Verify that the USB bus is in fact suspended, before we send
+        //remote wakeup signalling.
+        if(USBIsBusSuspended() == TRUE)
+        {
+            USBMaskInterrupts();
+            
+            //Clock switch to settings consistent with normal USB operation.
+            USBSuspendControl = 0; 
+            USBBusIsSuspended = FALSE;  //So we don't execute this code again, 
+                                        //until a new suspend condition is detected.
+
+            //Section 7.1.7.7 of the USB 2.0 specifications indicates a USB
+            //device must continuously see 5ms+ of idle on the bus, before it sends
+            //remote wakeup signalling.  One way to be certain that this parameter
+            //gets met, is to add a 2ms+ blocking delay here (2ms plus at 
+            //least 3ms from bus idle to USBIsBusSuspended() == TRUE, yeilds
+            //5ms+ total delay since start of idle).
+            do_delay(3600U);
+            
+            //Now drive the resume K-state signalling onto the USB bus.
+            USBResumeControl = 1;       // Start RESUME signaling
+            do_delay(1800U);        // Set RESUME line for 1-13 ms
+            USBResumeControl = 0;       //Finished driving resume signalling
+
+            USBUnmaskInterrupts();
+        }
+    }
+}
+
+
+/*******************************************************************
+ * Function:        BOOL USER_USB_CALLBACK_EVENT_HANDLER(
+ *                        USB_EVENT event, void *pdata, WORD size)
+ *
+ * PreCondition:    None
+ *
+ * Input:           USB_EVENT event - the type of event
+ *                  void *pdata - pointer to the event data
+ *                  WORD size - size of the event data
+ *
+ * Output:          None
+ *
+ * Side Effects:    None
+ *
+ * Overview:        This function is called from the USB stack to
+ *                  notify a user application that a USB event
+ *                  occured.  This callback is in interrupt context
+ *                  when the USB_INTERRUPT option is selected.
+ *
+ * Note:            None
+ *******************************************************************/
+BOOL USER_USB_CALLBACK_EVENT_HANDLER(int event, void *pdata, WORD size)
+{
+    /*
+    if-statement, rather than switch(), is used here as Microchip's compiler can be very inefficient with switch()
+    */
+    if (EVENT_SOF == event)
+    {
+#ifndef OMIT_HIDHYBRID
+        /* flag main loop to service millisecond tick */
+        msTick = TRUE;
+#endif
+    }
+    else if (EVENT_CONFIGURED == event)
+    {
+        //enable the HID endpoint
+        USBEnableEndpoint(HID_EP, USB_IN_ENABLED | USB_OUT_ENABLED | USB_HANDSHAKE_ENABLED | USB_DISALLOW_SETUP);
+        //Re-arm the OUT endpoint for the next packet
+        USBOutHandle = HIDRxPacket(HID_EP,(BYTE*)&RxDataBuffer,64);
+    }
+    else if (EVENT_EP0_REQUEST == event)
+    {
+        USBCheckHIDRequest();
+    }
+    return TRUE; 
+}
+
+static void do_delay(WORD delay_count)
+{
+    do
+    {
+        delay_count--;
+    }while(delay_count);
+}
---
+++ bootloader/hidplus_descriptors.c
@@ -0,0 +1,226 @@
+/*
+    USB HID bootloader for PIC16F1454/PIC16F1455/PIC16F1459 microcontroller
+
+    Copyright (C) 2013 Peter Lawrence
+
+    Permission is hereby granted, free of charge, to any person obtaining a 
+    copy of this software and associated documentation files (the "Software"), 
+    to deal in the Software without restriction, including without limitation 
+    the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+    and/or sell copies of the Software, and to permit persons to whom the 
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in 
+    all copies or substantial portions of the Software.
+
+    The software is for use solely and exclusively on Microchip PIC 
+    Microcontroller products. 
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+    DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+    modify the USB descriptors below to suit your application
+*/
+
+/* silly Microchip USB stack convention; C files should righfully NOT be coded this way */
+#ifndef __USB_DESCRIPTORS_C
+#define __USB_DESCRIPTORS_C
+
+#include "./USB/usb.h"
+#include "./USB/usb_function_hid.h"
+
+/* Device Descriptor for bootloader */
+ROM USB_DEVICE_DESCRIPTOR hid_bootloader_device_dsc=
+{
+    0x12,    // Size of this descriptor in bytes
+    USB_DESCRIPTOR_DEVICE,                // DEVICE descriptor type
+    0x0200,                 // USB Spec Release Number in BCD format
+    0x00,                   // Class Code
+    0x00,                   // Subclass code
+    0x00,                   // Protocol code
+    USB_EP0_BUFF_SIZE,          // Max packet size for EP0, see usb_config.h
+    0x04D8,                 // Vendor ID
+    0x003F,                 // Product ID: Custom HID device demo
+    0x0002,                 // Device release number in BCD format
+    0x01,                   // Manufacturer string index
+    0x02,                   // Product string index
+    0x00,                   // Device serial number string index
+    0x01                    // Number of possible configurations
+};
+
+#ifndef OMIT_HIDHYBRID
+/* Device Descriptor for HIDHYBRID mode */
+ROM USB_DEVICE_DESCRIPTOR hid_cypher_device_dsc=
+{
+    0x12,    // Size of this descriptor in bytes
+    USB_DESCRIPTOR_DEVICE,                // DEVICE descriptor type
+    0x0200,                 // USB Spec Release Number in BCD format
+    0x00,                   // Class Code
+    0x00,                   // Subclass code
+    0x00,                   // Protocol code
+    USB_EP0_BUFF_SIZE,          // Max packet size for EP0, see usb_config.h
+    0x04D8,                 // Vendor ID
+    0x003F,                 // Product ID: Custom HID device demo
+    0x0002,                 // Device release number in BCD format
+    0x01,                   // Manufacturer string index
+    0x02,                   // Product string index
+    0x00,                   // Device serial number string index
+    0x01                    // Number of possible configurations
+};
+#endif
+
+/* Configuration 1 Descriptor */
+ROM BYTE configDescriptor1[]=
+{
+    /* Configuration Descriptor */
+    0x09,//sizeof(USB_CFG_DSC),    // Size of this descriptor in bytes
+    USB_DESCRIPTOR_CONFIGURATION,                // CONFIGURATION descriptor type
+    0x29,0x00,            // Total length of data for this cfg
+    1,                      // Number of interfaces in this cfg
+    1,                      // Index value of this configuration
+    0,                      // Configuration string index
+    _DEFAULT | _SELF,               // Attributes, see usb_device.h
+    50,                     // Max power consumption (2X mA)
+							
+    /* Interface Descriptor */
+    0x09,//sizeof(USB_INTF_DSC),   // Size of this descriptor in bytes
+    USB_DESCRIPTOR_INTERFACE,               // INTERFACE descriptor type
+    0,                      // Interface Number
+    0,                      // Alternate Setting Number
+    2,                      // Number of endpoints in this intf
+    HID_INTF,               // Class code
+    0,     // Subclass code
+    0,     // Protocol code
+    0,                      // Interface string index
+
+    /* HID Class-Specific Descriptor */
+    0x09,//sizeof(USB_HID_DSC)+3,    // Size of this descriptor in bytes
+    DSC_HID,                // HID descriptor type
+    0x11,0x01,                 // HID Spec Release Number in BCD format (1.11)
+    0x00,                   // Country Code (0x00 for Not supported)
+    HID_NUM_OF_DSC,         // Number of class descriptors, see usbcfg.h
+    DSC_RPT,                // Report descriptor type
+    HID_RPT01_SIZE,0x00,//sizeof(hid_rpt01),      // Size of the report descriptor
+    
+    /* Endpoint Descriptor */
+    0x07,/*sizeof(USB_EP_DSC)*/
+    USB_DESCRIPTOR_ENDPOINT,    //Endpoint Descriptor
+    HID_EP | _EP_IN,                   //EndpointAddress
+    _INTERRUPT,                       //Attributes
+    0x40,0x00,                  //size
+    0x01,                        //Interval
+
+    /* Endpoint Descriptor */
+    0x07,/*sizeof(USB_EP_DSC)*/
+    USB_DESCRIPTOR_ENDPOINT,    //Endpoint Descriptor
+    HID_EP | _EP_OUT,                   //EndpointAddress
+    _INTERRUPT,                       //Attributes
+    0x40,0x00,                  //size
+    0x01                        //Interval
+};
+
+//Language code string descriptor
+static ROM struct
+{
+    BYTE bLength;
+    BYTE bDscType;
+    WORD string[1];
+} sd000 =
+{
+    sizeof(sd000),
+    USB_DESCRIPTOR_STRING,
+    {0x0409}
+};
+
+//Manufacturer string descriptor
+static ROM struct
+{
+    BYTE bLength;
+    BYTE bDscType;
+    WORD string[4];
+} sd001 =
+{
+    sizeof(sd001),
+    USB_DESCRIPTOR_STRING,
+    {'A','c','m','e'}
+};
+
+static ROM struct
+{
+    BYTE bLength;
+    BYTE bDscType;
+    WORD string[10];
+} sd002 =
+{
+    sizeof(sd002),
+    USB_DESCRIPTOR_STRING,
+    {'b','o','o','t','l','o','a','d','e','r'}
+};
+
+#ifndef OMIT_HIDHYBRID
+static ROM struct
+{
+    BYTE bLength;
+    BYTE bDscType;
+    WORD string[6];
+} sd003 =
+{
+    sizeof(sd003),
+    USB_DESCRIPTOR_STRING,
+    {'C','y','p','h','e','r'}
+};
+#endif
+
+//Class specific descriptor - HID 
+ROM struct
+{
+    BYTE report[HID_RPT01_SIZE];
+} hid_rpt01 = 
+{
+    {
+        0x06, 0x00, 0xFF,       // Usage Page = 0xFF00 (Vendor Defined Page 1)
+        0x09, 0x01,             // Usage (Vendor Usage 1)
+        0xA1, 0x01,             // Collection (Application)
+        0x19, 0x01,             //      Usage Minimum 
+        0x29, 0x40,             //      Usage Maximum 	//64 input usages total (0x01 to 0x40)
+        0x15, 0x01,             //      Logical Minimum (data bytes in the report may have minimum value = 0x00)
+        0x25, 0x40,      	  	//      Logical Maximum (data bytes in the report may have maximum value = 0x00FF = unsigned 255)
+        0x75, 0x08,             //      Report Size: 8-bit field size
+        0x95, 0x40,             //      Report Count: Make sixty-four 8-bit fields (the next time the parser hits an "Input", "Output", or "Feature" item)
+        0x81, 0x00,             //      Input (Data, Array, Abs): Instantiates input packet fields based on the above report size, count, logical min/max, and usage.
+        0x19, 0x01,             //      Usage Minimum 
+        0x29, 0x40,             //      Usage Maximum 	//64 output usages total (0x01 to 0x40)
+        0x91, 0x00,             //      Output (Data, Array, Abs): Instantiates output packet fields.  Uses same report size and count as "Input" fields, since nothing new/different was specified to the parser since the "Input" item.
+        0xC0
+    }
+};                  
+
+ROM BYTE *ROM HID_USB_CD_Ptr[]=
+{
+    (ROM BYTE *ROM)&configDescriptor1
+};
+
+ROM BYTE *ROM HID_USB_SD_Bootloader_Ptr[USB_NUM_STRING_DESCRIPTORS] =
+{
+    (ROM BYTE *ROM)&sd000,
+    (ROM BYTE *ROM)&sd001,
+    (ROM BYTE *ROM)&sd002
+};
+
+#ifndef OMIT_HIDHYBRID
+ROM BYTE *ROM HID_USB_SD_Cypher_Ptr[USB_NUM_STRING_DESCRIPTORS] =
+{
+    (ROM BYTE *ROM)&sd000,
+    (ROM BYTE *ROM)&sd001,
+    (ROM BYTE *ROM)&sd003
+};
+#endif
+
+#endif
